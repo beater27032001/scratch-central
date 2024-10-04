2e15e9fecfd0d100c2d730798cd75f85984faa98
José Carlos <josecarlospaivasantos2001@gmail.com>
2024-10-04 14:47:24 -0300

    ,

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import GameList from "@/components/GameList";
 import Header from "@/components/Header";
-import Image from "next/image";
 import games from "../../data/games.json";
 
 export default function Home() {
